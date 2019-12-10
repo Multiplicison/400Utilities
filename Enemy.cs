a9c7f1141f14e7392e7a59c668bcf9fd1f321034
Multiplicison <46792534+Multiplicison@users.noreply.github.com>
2019-12-09 21:02:24 -0700

    Script uploads
    Uploading Python notebooks and useful CS scripts I've referenced over this semester.
    Also included XInputDotNetAdapter.cs, which is an interesting script for handling new controller input, given for public use by GitHub user daemon3000.

---
+++ Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    private NavMeshAgent Foe;
+
+    public GameObject Player;
+
+    public float EnemyDistance = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Foe = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        float rayDistance;
+        Vector3 rayForward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, rayForward, Color.green);
+
+        //Recognize Player(s):
+        if(Physics.Raycast(transform.position,(rayForward), out hit))
+        {
+            if(hit.collider.gameObject.name == "EthanPrefab(Clone)")
+            {
+                Player = hit.collider.gameObject;
+            }
+        }
+
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        // Follow player
+
+        if(distance < EnemyDistance)
+        {
+            Vector3 directToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - directToPlayer;
+
+            Foe.SetDestination(newPos);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public Text countText;
+    public Text winText;
+
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 2)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
